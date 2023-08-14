e564144217f6a6abb21f63dab15342a324be83f4
ch_wannaporn <ch.wannaporn_st@tni.ac.th>
2023-08-14 18:56:31 +0700

    add tailwind

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class HelloWorld extends Vue {
+  msg!: string
+}
+</script>
