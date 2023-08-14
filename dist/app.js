09b39124e1710e79d58447893d8e5efdbc298771
ch_wannaporn <ch.wannaporn_st@tni.ac.th>
2023-08-14 15:01:04 +0700

    adjust footer

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_ejs_layouts_1 = __importDefault(require("express-ejs-layouts"));
 const home_controller_1 = __importDefault(require("./controllers/home.controller"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.use(express_1.default.static('public'));
-app.set('view engine', 'ejs');
-app.use('/', home_controller_1.default);
+app.use(express_1.default.static("public"));
+app.set("view engine", "ejs");
+app.use(express_ejs_layouts_1.default);
+app.set("layout", "components/layout");
+app.use("/", home_controller_1.default);
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import HomeController from './controllers/home.controller'
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+
+import HomeController from "./controllers/home.controller";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "components/layout");
 
-app.use('/', HomeController);
+app.use("/", HomeController);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
