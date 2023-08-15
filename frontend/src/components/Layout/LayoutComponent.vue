a9bb94dd64d5b30b5e33020402f0332e18af6763
ch_wannaporn <ch.wannaporn_st@tni.ac.th>
2023-08-15 15:40:02 +0700

    create layout

---
+++ frontend/src/components/Layout/LayoutComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col items-center bg-rose-50">
+        <HeaderComponent/>
+            <slot/>
+        <FooterComponent/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeaderComponent from './HeaderComponent.vue';
+import FooterComponent from './FooterComponent.vue';
+
+export default defineComponent({
+    name: 'LayoutComponent',
+    components: { HeaderComponent, FooterComponent }
+})
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="flex flex-col items-center bg-rose-50">
-    <HeaderComponent/>
+  <LayoutComponent>
     <BannerComponent/>
     <SearchBoxComponent/>
-    <FooterComponent/>
-  </div>
+  </LayoutComponent>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HeaderComponent from '@/components/HeaderComponent.vue';
-import FooterComponent from '@/components/FooterComponent.vue';
-import BannerComponent from '@/components/BannerComponent.vue';
-import SearchBoxComponent from '@/components/SearchBoxComponent.vue';
+import LayoutComponent from '@/components/Layout/LayoutComponent.vue';
+import BannerComponent from '@/components/Home/BannerComponent.vue';
+import SearchBoxComponent from '@/components/Home/SearchBoxComponent.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HeaderComponent,
-    FooterComponent,
+    LayoutComponent,
     BannerComponent,
     SearchBoxComponent
 },
