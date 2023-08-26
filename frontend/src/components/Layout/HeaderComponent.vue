223af6568568487f084cca86f579b5112954d6ba
ch_wannaporn <ch.wannaporn_st@tni.ac.th>
2023-08-26 20:15:59 +0700

    integrate auth0

--- frontend/src/components/Layout/HeaderComponent.vue
+++ frontend/src/components/Layout/HeaderComponent.vue
@@ -1,27 +1,48 @@
 <template>
-<nav class="flex w-full h-20 justify-center items-center bg-white">
-  <div class="flex w-full md:w-2/3 justify-between items-baseline">
-    <span class="text-2xl text-violet-500 font-semibold"
-      >Domestic Flight Booking</span
-    >
-    <div class="flex items-center space-x-4 font-semibold">
-      <span
-        class="cursor-pointer hover:text-rose-300 hover:underline hover:decoration-dashed hover:underline-offset-2"
-        >Log in</span
+  <nav class="flex w-full h-20 justify-center items-center bg-white">
+    <div class="flex w-full md:w-2/3 justify-between items-baseline">
+      <span class="text-2xl text-violet-500 font-semibold"
+        >Domestic Flight Booking</span
       >
-      <span
-        class="cursor-pointer hover:text-rose-300 hover:underline hover:decoration-dashed hover:underline-offset-2"
-        >Sign up</span
-      >
+      <div class="flex items-center font-semibold" v-if="isAuthenticated">
+        <span>Hi, {{ user?.name }}</span>
+        <span class="px-2">|</span>
+        <span
+          class="cursor-pointer hover:text-rose-300 hover:underline hover:decoration-dashed hover:underline-offset-2"
+          @click="logout"
+          >Logout</span
+        >
+      </div>
+      <div class="flex items-center space-x-4 font-semibold" v-else>
+        <span
+          class="cursor-pointer hover:text-rose-300 hover:underline hover:decoration-dashed hover:underline-offset-2"
+          @click="login"
+          >Log in</span
+        >
+        <span
+          class="cursor-pointer hover:text-rose-300 hover:underline hover:decoration-dashed hover:underline-offset-2"
+          >Sign up</span
+        >
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { useAuth0 } from "@auth0/auth0-vue";
 
 export default defineComponent({
-    name: 'HeaderComponent'
-})
+  name: "HeaderComponent",
+  setup: function () {
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+    return {
+      login: () => loginWithRedirect(),
+      logout: () => logout(),
+      user,
+      isAuthenticated,
+    };
+  },
+});
 </script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import { createAuth0 } from "@auth0/auth0-vue";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: process.env.VUE_APP_AUTH0_DOMAIN as string,
+    clientId: process.env.VUE_APP_AUTH0_CLIENTID as string,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+    cacheLocation: "localstorage",
+  })
+);
+
+app.use(store);
+app.use(router);
+app.mount("#app");
